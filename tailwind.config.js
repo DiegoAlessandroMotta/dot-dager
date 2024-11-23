966e3bd7e2af1f4dd8017f275fc3b9261503fdad
Xanderistme <xanderitsme@gmail.com>
2024-11-23 09:58:07 -0500

    Add something2

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,19 @@ export default {
         'meow-script': ['Meow Script', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'icon-skyblue': 'rgb(30, 156, 241)',
-        'icon-lightGreen': 'rgb(0, 184, 122)',
-        'icon-carmesi': 'rgb(249, 26, 130)',
+        primary: {
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          200: '#ddd6fe',
+          300: '#c4b5fd',
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+          800: '#5b21b6',
+          900: '#4c1d95',
+          950: '#2e1065',
+        },
         secondary: {
           50: '#f0f9ff',
           100: '#e0f2fe',
